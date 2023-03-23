c7b9c570fa3e4db5d861cefac08dbcd153122793
Jack Thacker <jackthacker89@gmail.com>
2023-03-23 10:49:20 +0000

    Project set up

---
+++ cohorts_table.sql
@@ -0,0 +1,5 @@
+CREATE TABLE cohort (
+  id SERIAL PRIMARY KEY,
+  cohort_name text,
+  start_date date
+);
---
+++ students_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE students (
+  id SERIAL PRIMARY KEY,
+  student_name text,
+-- The foreign key name is always {other_table_singular}_id
+  cohort_id int,
+  constraint fk_cohort foreign key(cohort_id)
+    references cohort(id)
+    on delete cascade
+);
