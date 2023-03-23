bec73aafb884e88862ede0aa2bab073e35f23aa3
Jack Thacker <jackthacker89@gmail.com>
2023-03-23 12:15:06 +0000

    Write add method to print cohort 2

--- app.rb
+++ app.rb
@@ -1,1 +1,11 @@
 require_relative 'lib/database_connection'
+require_relative 'lib/cohort_repository'
+
+DatabaseConnection.connect('student_directory_2')
+
+repo = CohortRepository.new
+cohort = repo.find_with_students(2)
+students = ""
+cohort.students.each {|student| students << "#{student.id}. #{student.student_name} "}
+
+puts "Cohort: #{cohort.cohort_name}, start date: #{cohort.start_date}, students: #{students}."
